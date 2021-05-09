8cfe9832ff1e4694188998298ef3007e0acb7625
Johnny Garcia <johnnygar98@hotmail.com>
2021-05-08 22:06:03 -0500

    Adding relations in user and rol models

--- rest-api/src/main/java/com/tecazuay/example/restapi/models/Rol.java
+++ rest-api/src/main/java/com/tecazuay/example/restapi/models/Rol.java
@@ -1,45 +1,56 @@
 package com.tecazuay.example.restapi.models;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 @Table(name = "roles")
 public class Rol extends Globals {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "rol_id")
-    private int rolId;    
-    private String nombre;
 
-    public Rol() {
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "rol_id")
+	private int rolId;
 
-    public Rol(int rolId, String nombre) {
-        this.rolId = rolId;
-        this.nombre = nombre;
-    }
+	@Column(nullable = false, length = 25)
+	private String nombre;
 
-    public int getRolId() {
-        return rolId;
-    }
+	@JsonBackReference(value = "rf_usuario_rol")
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "rol")
+	private List<Usuario> usuarios;
 
-    public void setRolId(int rolId) {
-        this.rolId = rolId;
-    }
+	public Rol() {
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public Rol(int rolId, String nombre) {
+		this.rolId = rolId;
+		this.nombre = nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public int getRolId() {
+		return rolId;
+	}
 
-    
+	public void setRolId(int rolId) {
+		this.rolId = rolId;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 }
--- rest-api/src/main/java/com/tecazuay/example/restapi/models/Usuario.java
+++ rest-api/src/main/java/com/tecazuay/example/restapi/models/Usuario.java
@@ -1,108 +1,131 @@
 package com.tecazuay.example.restapi.models;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 @Table(name = "usuarios")
-public class Usuario extends Globals {
+public class Usuario extends Globals implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "usuario_id")
-    private int personaId;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4115808525376597079L;
 
-    private String nombres;
-    private String apellidos;
-    private String correo;
-    private String password;
-    private String token;
-    private String telefono;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "usuario_id")
+	private int personaId;
 
-    @ManyToOne
-    @Column(name = "rol_id")
-    private Rol rol;
+	@Column(nullable = false, length = 100)
+	private String nombres;
 
-    public Usuario() {
-    }
+	@Column(nullable = false, length = 100)
+	private String apellidos;
 
-    public Usuario(int personaId, String nombres, String apellidos, String correo, String password, String token,
-            String telefono) {
-        this.personaId = personaId;
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.correo = correo;
-        this.password = password;
-        this.token = token;
-        this.telefono = telefono;
-    }
+	@Column(nullable = false, length = 50)
+	private String correo;
 
-    public int getPersonaId() {
-        return personaId;
-    }
+	@Column(nullable = false, length = 20)
+	private String password;
 
-    public void setPersonaId(int personaId) {
-        this.personaId = personaId;
-    }
+	@Column(nullable = false, length = 100)
+	private String token;
 
-    public String getNombres() {
-        return nombres;
-    }
+	@Column(nullable = false, length = 15)
+	private String telefono;
 
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
+	@JsonManagedReference(value = "rf_usuario_rol")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "rol_id", nullable = false)
+	private Rol rol;
 
-    public String getApellidos() {
-        return apellidos;
-    }
+	public Usuario() {
+	}
 
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
+	public Usuario(int personaId, String nombres, String apellidos, String correo, String password, String token,
+			String telefono) {
+		this.personaId = personaId;
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.correo = correo;
+		this.password = password;
+		this.token = token;
+		this.telefono = telefono;
+	}
 
-    public String getCorreo() {
-        return correo;
-    }
+	public int getPersonaId() {
+		return personaId;
+	}
 
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
+	public void setPersonaId(int personaId) {
+		this.personaId = personaId;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getNombres() {
+		return nombres;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
 
-    public String getToken() {
-        return token;
-    }
+	public String getApellidos() {
+		return apellidos;
+	}
 
-    public void setToken(String token) {
-        this.token = token;
-    }
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
 
-    public String getTelefono() {
-        return telefono;
-    }
+	public String getCorreo() {
+		return correo;
+	}
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
 
-    public Rol getRol() {
-        return rol;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setRol(Rol rol) {
-        this.rol = rol;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
 }
