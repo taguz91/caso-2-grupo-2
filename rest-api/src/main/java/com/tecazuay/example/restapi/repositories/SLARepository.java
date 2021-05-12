b533f5ab60c0ba3618d20b2111f21ac9bae74eee
Andrés <freddy.sapatanga.est@tecazuay.edu.ec>
2021-05-11 21:36:48 -0500

    Juas

--- rest-api/src/main/java/com/tecazuay/example/restapi/repositories/SLARepository.java
+++ rest-api/src/main/java/com/tecazuay/example/restapi/repositories/SLARepository.java
@@ -1,7 +1,10 @@
 package com.tecazuay.example.restapi.repositories;
 
+import java.util.List;
+
 import com.tecazuay.example.restapi.models.SLA;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
