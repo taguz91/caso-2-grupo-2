c9e4ddab03d65589034ae1a4aec3f11db1aef986
sebastian8080 <sebas25211@hotmail.com>
2021-05-14 21:58:43 -0500

    Terminando validaciones en Categoria y Servicio

--- rest-api/src/main/java/com/tecazuay/example/restapi/controllers/ServicioController.java
+++ rest-api/src/main/java/com/tecazuay/example/restapi/controllers/ServicioController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-<<<<<<< HEAD
 @RequestMapping("/api/v1/servicios")
 public class ServicioController {
     
@@ -48,7 +47,9 @@ public class ServicioController {
 
     @GetMapping("/categoria/{id}")
     public List <ServicioResponse> getAllByCategoriaId(@PathVariable("id") Long categoria_id){
-        return servicioRepository.findAllByCategoriaId(categoria_id);
+        CategoriaResponse categoria = categoriaRepository.findByCategoriaId(categoria_id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Esta categoria no esta registrada"));
+        return servicioRepository.findAllByCategoriaId(categoria.getCategoria_id());
     }
 
     @PostMapping("/")
@@ -66,34 +67,9 @@ public class ServicioController {
         Servicio servicio = servicioRepository.findById(servicio_id)
             .orElseThrow(() -> new ResourceNotFoundException("No se encontro el servicio con id: " + servicio_id));
         servicio.setNombre_servicio(newServicio.getNombre_servicio());
+        servicio.setCategoria(newServicio.getCategoria());
         return servicioRepository.save(servicio);
     }
-=======
-@RequestMapping(value = "/api/v1/servicios", consumes = MediaType.APPLICATION_JSON_VALUE)
-public class ServicioController {
-
-	@Autowired
-	private ServicioRepository servicioRepository;
-
-	@GetMapping(value = "/")
-	public List<Servicio> getAll() {
-		return servicioRepository.findAll();
-	}
-
-	@GetMapping("/{id}")
-	public List<Servicio> getAllByCategoriaId(@PathVariable("id") Long categoria_id) {
-		return servicioRepository.findByCategoriaId(categoria_id);
-	}
-
-	@PostMapping(value = "/")
-	@ResponseBody
-	public Servicio saveServicio(@RequestBody @Valid ServicioParam servicioParam) {
-		Servicio servicio = new Servicio();
-		servicio.setNombre_servicio(servicioParam.getNombre_servicio());
-		servicio.setCategoria(servicioParam.getCategoria());
-		return servicioRepository.save(servicio);
-	}
->>>>>>> 7803c7d5577bdb19efb32309f9b1315f7e460e0c
 
     @DeleteMapping("/{id}")
     public boolean deleteServicio(@PathVariable(value = "id") Long servicio_id){
