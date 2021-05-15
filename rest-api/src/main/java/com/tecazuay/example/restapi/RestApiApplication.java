c9d81d43f10b6063ed70c6ccea3ce75608b38bf3
Johnny Garcia <johnnygar98@hotmail.com>
2021-05-14 19:08:02 -0500

    Fixing errors

--- rest-api/src/main/java/com/tecazuay/example/restapi/RestApiApplication.java
+++ rest-api/src/main/java/com/tecazuay/example/restapi/RestApiApplication.java
@@ -8,9 +8,5 @@ public class RestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
-<<<<<<< HEAD
-    
-=======
->>>>>>> b12600446fe6f18b09220ef375639617cc036e63
 	}
 }
--- rest-api/src/main/java/com/tecazuay/example/restapi/controllers/ServicioController.java
+++ rest-api/src/main/java/com/tecazuay/example/restapi/controllers/ServicioController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-<<<<<<< HEAD
 @RequestMapping("/api/v1/servicios")
 public class ServicioController {
     
@@ -68,32 +67,6 @@ public class ServicioController {
         servicio.setNombre_servicio(newServicio.getNombre_servicio());
         return servicioRepository.save(servicio);
     }
-=======
-@RequestMapping(value = "/api/v1/servicios", consumes = MediaType.APPLICATION_JSON_VALUE)
-public class ServicioController {
-
-	@Autowired
-	private ServicioRepository servicioRepository;
-
-	@GetMapping(value = "/")
-	public List<Servicio> getAll() {
-		return servicioRepository.findAll();
-	}
-
-	@GetMapping("/{id}")
-	public List<Servicio> getAllByCategoriaId(@PathVariable("id") Long categoria_id) {
-		return servicioRepository.findByCategoriaId(categoria_id);
-	}
-
-	@PostMapping(value = "/")
-	@ResponseBody
-	public Servicio saveServicio(@RequestBody @Valid ServicioParam servicioParam) {
-		Servicio servicio = new Servicio();
-		servicio.setNombre_servicio(servicioParam.getNombre_servicio());
-		servicio.setCategoria(servicioParam.getCategoria());
-		return servicioRepository.save(servicio);
-	}
->>>>>>> 7803c7d5577bdb19efb32309f9b1315f7e460e0c
 
     @DeleteMapping("/{id}")
     public boolean deleteServicio(@PathVariable(value = "id") Long servicio_id){
