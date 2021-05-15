e60eef561ebc878a3f22789afc87b930d5bf2e48
Johnny Garcia <johnnygar98@hotmail.com>
2021-05-14 22:25:17 -0500

    Adding swagger documentation

--- rest-api/src/main/java/com/tecazuay/example/restapi/RestApiApplication.java
+++ rest-api/src/main/java/com/tecazuay/example/restapi/RestApiApplication.java
@@ -1,12 +1,30 @@
 package com.tecazuay.example.restapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @SpringBootApplication
 public class RestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+				.components(new Components().addSecuritySchemes("basicScheme",
+						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+				.info(new Info().title("Tirtec API").version(appVersion)
+						.description("This is a sample application, using JWT.")
+						.license(new License().name("Apache 2.0")));
 	}
 }
