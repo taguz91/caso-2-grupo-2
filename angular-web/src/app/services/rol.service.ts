d5349546d2166f26d163d003566ad4ec22f53be1
Andrés <freddy.sapatanga.est@tecazuay.edu.ec>
2021-05-15 23:04:43 -0500

    Rol, User models and services added, not tested

--- angular-web/src/app/services/rol.service.ts
+++ angular-web/src/app/services/rol.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { URL_BASE_V1, loadHeader } from '../utils/constantes';
+import { URL_BASE_V1, loadHeader, handleError } from '../utils/constantes';
 import { Rol } from '../models/rol';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +14,19 @@ export class RolService {
 
   constructor(private http: HttpClient) {}
 
-  readAllRoles(): Observable<Rol> {
-    return this.http.get<Rol>(`${this.BASE_URL}`, loadHeader());
+  readAllRoles(): Observable<Rol[]> {
+    return this.http.get<Rol[]>(`${this.BASE_URL}`, loadHeader())
+    .pipe(
+      tap((_) => console.log('Loading rol servicios')),
+      catchError(handleError<Rol[]>([]))
+    );
   }
 
-  readRolById(): Observable<Rol> {
-    return this.http.get<Rol>(`${this.BASE_URL}`, loadHeader());
+  readRolById(id: string): Observable<Rol> {
+    return this.http.get<Rol>(`${this.BASE_URL}${id}`, loadHeader())
+    .pipe(
+      tap((_) => console.log('Loading rol servicios')),
+      catchError(handleError<Rol>(null))
+    );
   }
 }
--- angular-web/src/app/services/usuario.service.ts
+++ angular-web/src/app/services/usuario.service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Usuario } from '../models/usuario';
-import { loadHeader, URL_BASE_V1 } from '../utils/constantes';
+import { handleError, loadHeader, URL_BASE_V1 } from '../utils/constantes';
+import { PageResponse } from '../models/parametros';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,27 +15,51 @@ export class UsuarioService {
 
   constructor(private http: HttpClient) {}
 
-  login(correo: string, password: string):Observable<Usuario> {
-    return this.http.post<Usuario>(`${this.BASE_URL}`, {'correo':correo, 'password': password}, loadHeader());
+  login(correo: string, password: string):Observable<any> {
+    return this.http.post<PageResponse<Usuario[]>>(`${this.BASE_URL}`, {'correo':correo, 'password': password}, loadHeader())
+    .pipe(
+      tap((_) => console.log('Loading usuario servicios')),
+      catchError(handleError<any>(null))
+    );
   }
 
   createUser(usuario: Usuario, rolId: string): Observable<Usuario> {
-    return this.http.post<Usuario>(`${this.BASE_URL}${rolId}`, usuario, loadHeader());
+    return this.http.post<Usuario>(`${this.BASE_URL}${rolId}`, usuario, loadHeader())
+    .pipe(
+      tap((_) => console.log('Loading usuario servicios')),
+      catchError(handleError<Usuario>(null))
+    );
   }
 
-  readAllUsers(): Observable<Usuario>   {
-    return this.http.get<Usuario>(`${this.BASE_URL}`, loadHeader());
+  readAllUsers(): Observable<PageResponse<Usuario[]>>   {
+    return this.http.get<PageResponse<Usuario[]>>(`${this.BASE_URL}`, loadHeader())
+    .pipe(
+      tap((_) => console.log('Loading usuario servicios')),
+      catchError(handleError<PageResponse<Usuario[]>>(null))
+    );
   }
 
   readUserById(id: string): Observable<Usuario> {
-    return this.http.get<Usuario>(`${this.BASE_URL}${id}`, loadHeader());
+    return this.http.get<Usuario>(`${this.BASE_URL}${id}`, loadHeader())
+    .pipe(
+      tap((_) => console.log('Loading usuario servicios')),
+      catchError(handleError<Usuario>(null))
+    );
   }
 
   updateUser(usuario: Usuario): Observable<Usuario> {
-    return this.http.put<Usuario>(`${this.BASE_URL}`, usuario, loadHeader());
+    return this.http.put<Usuario>(`${this.BASE_URL}`, usuario, loadHeader())
+    .pipe(
+      tap((_) => console.log('Loading usuario servicios')),
+      catchError(handleError<Usuario>(null))
+    );
   }
 
   updateUserRol(personaId: string, rolId: string): Observable<Usuario> {
     return this.http.put<Usuario>(`${this.BASE_URL}${personaId}/${rolId}`, null, loadHeader())
+    .pipe(
+      tap((_) => console.log('Loading usuario servicios')),
+      catchError(handleError<Usuario>(null))
+    );
   }
 }
