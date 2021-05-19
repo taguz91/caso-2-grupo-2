8d62d2e1040e9ca485c155569ac1f82fefd7dad9
Johnny Garcia <johnnygar98@hotmail.com>
2021-05-19 01:13:33 -0500

    Ticket id unique in encuesta service

--- rest-api/src/main/java/com/tecazuay/example/restapi/models/EncuestaSatisfacion.java
+++ rest-api/src/main/java/com/tecazuay/example/restapi/models/EncuestaSatisfacion.java
@@ -33,8 +33,8 @@ public class EncuestaSatisfacion extends Globals implements Serializable {
 	private String comentario;
 	
 	@JsonBackReference(value = "rf_ticket_encuesta_satisfaccion")
-	@JoinColumn(name = "ticket_id", nullable = false)
-	@OneToOne(cascade = CascadeType.ALL, mappedBy = "encuesta")
+	@JoinColumn(name = "ticket_id", nullable = false, unique = true)
+	@OneToOne(cascade = CascadeType.ALL)
 	private Ticket ticket;
 
 	public Long getEncuesta_id() {
