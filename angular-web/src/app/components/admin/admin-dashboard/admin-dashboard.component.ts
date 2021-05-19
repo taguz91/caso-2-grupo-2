327d086a7290facb33bba97a4730f737eb6a9214
Johnny Garcia <johnnygar98@hotmail.com>
2021-05-18 23:38:34 -0500

    Fix errors

--- angular-web/src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
+++ angular-web/src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
@@ -7,21 +7,9 @@ import { BreadcrumbService } from 'src/app/services/breadcrumb.service';
   styleUrls: ['./admin-dashboard.component.scss'],
 })
 export class AdminDashboardComponent implements OnInit {
-<<<<<<< HEAD
-
-
-  constructor() { }
-
-  ngOnInit(): void {
-    
-  }
-
-
-=======
   constructor(private breadcrumb: BreadcrumbService) {}
 
   ngOnInit(): void {
     this.breadcrumb.addRutes([]);
   }
->>>>>>> d13e952ec32e47d1ba0c739f016fd564172ee741
 }
