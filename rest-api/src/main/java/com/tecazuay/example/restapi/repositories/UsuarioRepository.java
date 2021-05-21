61ec8cfb5cc4927cccfabcc5a8a019bd1a282169
Johnny Garcia <johnnygar98@hotmail.com>
2021-05-21 18:39:18 -0500

    Adding new service for list user types

--- rest-api/src/main/java/com/tecazuay/example/restapi/repositories/UsuarioRepository.java
+++ rest-api/src/main/java/com/tecazuay/example/restapi/repositories/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package com.tecazuay.example.restapi.repositories;
 
+import com.tecazuay.example.restapi.definitions.UserList;
 import com.tecazuay.example.restapi.models.Usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -22,5 +24,8 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	@Query(value = "SELECT u FROM usuarios u")
 	Page<Usuario> findAllPage(Pageable pageable);
+
+	@Query(value = "SELECT usuario_id, nombres || ' ' || apellidos AS nombre_completo FROM usuarios WHERE rol_id = :idRol", nativeQuery = true)
+	List<UserList> findAllComboByUserType(Long idRol);
 
 }
