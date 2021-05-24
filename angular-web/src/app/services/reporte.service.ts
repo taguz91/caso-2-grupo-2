4d461d671be8171fe3be4d95bc18d91f4fde97af
Andrés <freddy.sapatanga.est@tecazuay.edu.ec>
2021-05-23 22:07:24 -0500

    Arreglo reportes

--- angular-web/src/app/services/reporte.service.ts
+++ angular-web/src/app/services/reporte.service.ts
@@ -1,18 +1,17 @@
-import { Injectable, Optional } from '@angular/core';
+import { Injectable } from '@angular/core';
 import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReporteService {
 
-  constructor() { }
+  constructor() {}
 
   reporte(data : string) {
+    
     var DATA: any = document.getElementById(data);
 
-    /*
     var doc = new jsPDF(
       {
         orientation: '1',
@@ -26,7 +25,5 @@ export class ReporteService {
     doc.text("Instituto Tecnologico Superior del Azuay", 180,20);
     doc.fromHTML(DATA,10,25);
     doc.save("IstaReporte.pdf");
-    */
   }
-
 }
