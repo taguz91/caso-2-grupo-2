1c37b546fe76b4c4aee44afffb9e9316b9556656
Alvaro-afk-25 <alvaro.guaman.est@tecazuay.edu.ec>
2021-05-24 16:09:22 -0500

    Add new style of the report's

--- angular-web/src/app/services/reporte.service.ts
+++ angular-web/src/app/services/reporte.service.ts
@@ -14,40 +14,23 @@ export class ReporteService {
   reporte(data : string) {
     var DATA: any = document.getElementById(data);
 
-    var pdf = new jsPDF({
-      orientation: 'l',
-      unit :'pt',
-      format: 'A4',
-      position:2
-    });
-    this.img.src=('/assets/img/logo.png') ;
-    pdf.addImage(this.img,'PNG',10,10,80,70);
-    pdf.text(290, 50, 'Instituto Tecnologico Superior del Azuay');
-    pdf.fromHTML(DATA,60,100);
-    pdf.setFontSize(10);
-    pdf.setFont("",10);
-    pdf.save("mensahe.pdf");
-    /*const DATA: any = document.getElementById(data);
-    const doc = new jsPDF('p', 'pt', 'a4');
-    const options = {
-      background: 'white',
-      scale: 3
-    };
+    var pdf = new jsPDF('l', 'pt', 'a4');
 
-    html2canvas(DATA, options).then((canvas) => {
-
+    html2canvas(DATA).then((canvas) => {
+      this.img.src=('/assets/img/logo.png') ;
+      pdf.addImage(this.img,'PNG',10,10,80,70);
+      pdf.text(290, 50, 'Instituto Tecnologico Superior del Azuay');
       const img = canvas.toDataURL('image/PNG');
-
-      // Add image Canvas to PDF
+      console.log(img);
       const bufferX = 15;
-      const bufferY = 15;
-      const imgProps = (doc as any).getImageProperties(img);
-      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const bufferY = 85;
+      const imgProps = (pdf as any).getImageProperties(img);
+      const pdfWidth = 800;
       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, 'Slow', 'FAST');
-      return doc;
+      pdf.addImage(img, 'PNG',  bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'SLOW');
+      return pdf;
     }).then((docResult) => {
-      docResult.save(`${new Date().toISOString()}_TDS.pdf`);
-    });*/
+      docResult.save(`${new Date().toISOString()}_TIRTEC.pdf`);
+    });
   }
 }
